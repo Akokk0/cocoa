feea552b2d3ecd9d44201e45472318f951467004
Akokko <admin@akokko.com>
2024-04-27 17:22:15 +0800

    style:Code format

--- src/components/home/tabsarea/slide.tsx
+++ src/components/home/tabsarea/slide.tsx
@@ -87,21 +87,19 @@ export default function Slide({
     return (
         <div className="fresh-home-video-slides">
             <div className="fresh-home-video-slides-covers" ref={containerRef}>
-                {
-                    items.map((item, index) => {
-                        return (
-                            <a data-id={item.aid} key={item.aid} onClick={e => {
-                                e.preventDefault()
-                                if (index === 1) {
-                                    openPlayer(item.bvid, item.cid)
-                                }
-                                jumpToCard(index)
-                            }} href='#' className="fresh-home-video-slides-cover">
-                                <Image className="object-cover object-center" url={item.pic} alt="封面" />
-                            </a>
-                        )
-                    })
-                }
+                {items.map((item, index) => {
+                    return (
+                        <a data-id={item.aid} key={item.aid} onClick={e => {
+                            e.preventDefault()
+                            if (index === 1) {
+                                openPlayer(item.bvid, item.cid)
+                            }
+                            jumpToCard(index)
+                        }} href='#' className="fresh-home-video-slides-cover">
+                            <Image className="object-cover object-center" url={item.pic} alt="封面" />
+                        </a>
+                    )
+                })}
             </div>
             <div className="cover-placeholder-vertical"></div>
             <div className='fresh-home-video-slides-row'>
@@ -113,37 +111,37 @@ export default function Slide({
                                 <div className="flex items-center justify-center space-x-2 text-white bg-bili_blue rounded-full w-24 h-8 text-sm hover:cursor-pointer" onClick={() => openPlayer(items[1].bvid, items[1].cid)}>
                                     <Play className='w-4 h-4' /> <span>播放</span>
                                 </div>
-                            <div className="border border-bili_grey p-1 rounded-full text-gray-800">
-                                <Clock />
+                                <div className="border border-bili_grey p-1 rounded-full text-gray-800">
+                                    <Clock />
+                                </div>
+                            </div>
+                            <div>
+                                <UPInfo up={items[1].owner} />
                             </div>
                         </div>
-                        <div>
-                            <UPInfo up={items[1].owner} />
-                        </div>
                     </div>
+                    <a target="_blank" className="fresh-home-video-slides-main-title">
+                        {items[1].title}
+                    </a>
                 </div>
-                <a target="_blank" className="fresh-home-video-slides-main-title">
-                    {items[1].title}
-                </a>
-            </div>
-            <div className='fresh-home-video-slides-main-description scrollbar-hide'>
-                <div className='description-text w-72'>{items[1].desc}</div>
-            </div>
-            <div className="fresh-home-video-slides-actions">
-                <button className="absolute right-[7.1rem] bottom-4 rounded-full border flex items-center justify-center p-1 w-8 h-8 transition
+                <div className='fresh-home-video-slides-main-description scrollbar-hide'>
+                    <div className='description-text w-72'>{items[1].desc}</div>
+                </div>
+                <div className="fresh-home-video-slides-actions">
+                    <button className="absolute right-[7.1rem] bottom-4 rounded-full border flex items-center justify-center p-1 w-8 h-8 transition
                             rotatingF hover:border-bili_blue" onClick={getNewVideoDate}>
-                    <RotateCw className="w-5 h-5 rotatingElement" />
-                </button>
-                <button className="absolute right-[4.5rem] bottom-4 rounded-full border flex items-center justify-center p-1 w-8 h-8 transition
+                        <RotateCw className="w-5 h-5 rotatingElement" />
+                    </button>
+                    <button className="absolute right-[4.5rem] bottom-4 rounded-full border flex items-center justify-center p-1 w-8 h-8 transition
                             popLeftArrow hover:border-bili_blue" onClick={handleClicPre} >
-                    <ArrowLeft className="arrow-left" />
-                </button>
-                <button className="absolute right-4 bottom-4 flex items-center justify-center rounded-full border p-1 w-12 h-12 transition
+                        <ArrowLeft className="arrow-left" />
+                    </button>
+                    <button className="absolute right-4 bottom-4 flex items-center justify-center rounded-full border p-1 w-12 h-12 transition
                             popRightArrow hover:border-bili_blue" onClick={handleClicNext} >
-                    <ArrowRight className="w-8 h-8 arrow-right" />
-                </button>
+                        <ArrowRight className="w-8 h-8 arrow-right" />
+                    </button>
+                </div>
             </div>
-        </div>
         </div >
     )
 }
